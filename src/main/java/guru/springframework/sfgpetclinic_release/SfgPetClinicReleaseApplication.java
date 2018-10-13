e96743ccc7153792936e55ad9f5d36c9caa9bde7
codingObj <kscodingpractice@gmail.com>
2018-10-13 08:08:13 +0700

    Initial Project Add

---
+++ src/main/java/guru/springframework/sfgpetclinic_release/SfgPetClinicReleaseApplication.java
@@ -0,0 +1,12 @@
+package guru.springframework.sfgpetclinic_release;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgPetClinicReleaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgPetClinicReleaseApplication.class, args);
+    }
+}
